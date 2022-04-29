826f010881530999ebcd8a4569116b72c439af6d
jovic03 <42389786+jovic03@users.noreply.github.com>
2022-04-29 20:27:27 -0300

    alteracao de porta pra nao conflitar com front

--- ElGeladonVersao2-BK/index.js
+++ ElGeladonVersao2-BK/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 
-const port = 3000;
+const port = 3333;
 const app = express();
 
 const route = require('./src/routes/paletas.routes');
